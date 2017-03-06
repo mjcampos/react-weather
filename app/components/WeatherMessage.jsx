2aaf69779097a00057f00fabea1643ab93e650e0
Michael Campos <camposmj89@gmail.com>
2017-03-06 06:21:31 -0800

    Refactored code to use arrows, which allegedly simplifies it. Included readme.md

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,13 +1,9 @@
 var React = require('react');
 
-var WeatherMessage = React.createClass({
-	render: function() {
-		var {location, temp} = this.props;
-
-		return (
-			<h4>It is {temp} in {location}</h4>
-		);
-	}
-});
+var WeatherMessage = ({temp, location}) => {
+	return (
+		<h4>It is {temp} in {location}</h4>
+	);
+};
 
 module.exports = WeatherMessage;
